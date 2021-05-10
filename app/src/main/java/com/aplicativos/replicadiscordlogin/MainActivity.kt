ad4b4be607136257b8671ee67e7e8fb723f41b64
Gustavo <gggamer1511@gmail.com>
2021-05-10 20:45:55 -0300

    done.

--- app/src/main/java/com/aplicativos/replicadiscordlogin/MainActivity.kt
+++ app/src/main/java/com/aplicativos/replicadiscordlogin/MainActivity.kt
@@ -1,11 +1,17 @@
 package com.aplicativos.replicadiscordlogin
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun loginOnClick(view: View) {
+        startActivity(Intent(this, MainScreen::class.java))
     }
 }
